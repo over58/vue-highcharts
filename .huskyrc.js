f1ca8b474cbcd67c7f8a383f8d0aa5aad7a61279
yongchao9 <2658553345@qq.com>
2019-10-10 18:00:29 +0800

    添加husky提升工作流效率

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,11 +3,9 @@ const tasks = arr => arr.join(' && ')
 module.exports = {
   hooks: {
     'pre-commit': tasks([
-      'echo "开始啦"',
+      'echo "commit开始之前要做的事儿"',
       'npm run lint',
-      'npm run test',
-      'npm run build',
-      'git add -A'
+      'npm run build'
     ])
   }
 }
